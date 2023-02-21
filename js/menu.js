76718524f742cb1ccc37493b3454aa7744a6583b
tuoyuxuan <tuoyuxuan@acoinfo.com>
2023-02-21 14:53:38 +0800

    home commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> HOME</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> WORKS</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> ARTICLES</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> TAG</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> CLASSIFICATION</a>");
+document.writeln("<li style=\"--i: 5\"><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> FRIENDS</a></li>");
+document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> ABOUT</a></li>");
+document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> RavelloH.<br> All rights reserved.</b>");
+document.writeln("<br><br><a class=\'info\' style=\'width:88%;margin:0\' href=\'//ravelloh.gitee.io/cname"+window.location.pathname+"\'>当前源:<wbr><strong>"+window.location.hostname+"</strong><br>点击更换源</a>");
