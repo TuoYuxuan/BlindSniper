1d24e3d40deeafd0bfa8c28a76725485dd3b7786
tuoyuxuan <tuoyuxuan@acoinfo.com>
2023-02-27 11:46:37 +0800

    menu bug fixed

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> HOME</a></li>");
-document.writeln("<li style=\"--i: 1\"><a href=\'/sports/\'><span class=\'iconfontsmall icon-app\'></span> SPORTS</a></li>");
-document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> ARTICLES</a></li>");
-document.writeln("<li style=\"--i: 3\"><a href=\'/confessions/\'><span class=\'iconfontsmall icon-tag\'></span> CONFESSIONS</a></li>");
-document.writeln("<li style=\"--i: 4\"><a href=\'/jounrney/\'><span class=\'iconfontsmall icon-classification\'></span> JOURNEY</a>");
-document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> ABOUT</a></li>");
+document.writeln("<li style=\"--i: 0\"><a href=\'/BlindSniper/index.html\'><span class=\'iconfontsmall icon-home\'></span> HOME</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/BlindSniper/sports/\'><span class=\'iconfontsmall icon-app\'></span> SPORTS</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/BlindSniper/articles/\'><span class=\'iconfontsmall icon-article\'></span> ARTICLES</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/BlindSniper/confessions/\'><span class=\'iconfontsmall icon-tag\'></span> CONFESSIONS</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/BlindSniper/journey/\'><span class=\'iconfontsmall icon-classification\'></span> JOURNEY</a>");
+document.writeln("<li style=\"--i: 6\"><a href=\'/BlindSniper/about/\'><span class=\'iconfontsmall icon-about\'></span> ABOUT</a></li>");
 document.writeln("<b><br><br>Copyright © 2023 - <b id=\'year\'></b> TuoYuxuan.<br> All rights reserved.</b>");
 document.writeln("<br><br><a class=\'info\' style=\'width:88%;margin:0\' href=\'//tuoyuxuan.gitee.io/cname"+window.location.pathname+"\'>当前源:<wbr><strong>"+window.location.hostname+"</strong><br>点击更换源</a>");
